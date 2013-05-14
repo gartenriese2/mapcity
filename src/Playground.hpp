e69116cc1b429fc51d0a53fac86397c567954658
gartenriese <bastian.mattes@informatik.stud.uni-erlangen.de>
2013-05-14 20:22:55 +0200

    added Playground

---
+++ src/Playground.hpp
@@ -0,0 +1,15 @@
+#ifndef PLAYGROUND_HPP
+#define PLAYGROUND_HPP
+
+#include "../libace/libace.h"
+
+class Playground {
+	public:
+		Playground();
+		~Playground() {}
+		
+	private:
+		
+};
+
+#endif
