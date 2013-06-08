99d52665eb5c1f5f4b1d445c687969939b4f70dd
jandoerntlein <jan.doerntlein@informatik.stud.uni-erlangen.de>
2013-06-08 17:42:15 +0200

    mapcity Mac update

--- src/ogl.h
+++ src/ogl.h
@@ -1,8 +1,7 @@
 // OpenGL 
 #ifdef __MACH__
-    #include <OpenGL/gl.h>
-    #include <OpenGL/glu.h>
     #include <GL/glfw.h>
+    #include <OpenGL/gl3.h>
 #elif __linux__ 
     #include <GL/glew.h>
     #include <GL/glfw.h>
