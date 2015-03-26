19912a93757b0f58efd3bdb3f2803c2b5b18350c
Philipp Kögel <philippkoegel@web.de>
2015-03-26 12:43:50 +0100

    Auto run cmake when adding/removing files

--- run.sh
+++ run.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+PROJ_FILE=./.mapcity.files
+
+find ./src -type f \( -iname \*.cpp -o -iname \*.hpp \) > "${PROJ_FILE}.tmp"
+diff "${PROJ_FILE}" "${PROJ_FILE}.tmp" &> /dev/null
+if [ $? -ne 0 ]; then
+    make clean
+fi
+
+mv "${PROJ_FILE}.tmp" "${PROJ_FILE}"
+
 make && ./bin/mapcity
