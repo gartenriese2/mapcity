9546136c58c622445e560728897acd024572fafa
gartenriese <gartenriese2@users.noreply.github.com>
2015-04-11 17:38:30 +0200

    almost done with simple curves

--- src/main.cpp
+++ src/main.cpp
@@ -98,26 +98,26 @@ void demo0(ObjectManager & objManager, const std::unique_ptr<core::Input> & inpu
 }
 
 void demo1(ObjectManager & objManager, DrawableManager & drawableManager) {
-	auto smallID = objManager.add<StraightStreet>(glm::vec3(-50, -25, 0.01f), glm::vec3(0, -25, 0.01f), "SmallStreet");
-	auto mediumID = objManager.add<StraightStreet>(glm::vec3(0, -25, 0.02f), glm::vec3(25, 0, 0.02f), "MediumStreet");
-	auto smallPtr = std::dynamic_pointer_cast<StraightStreet>(objManager.get(smallID));
-	auto mediumPtr = std::dynamic_pointer_cast<StraightStreet>(objManager.get(mediumID));
-	objManager.add<Intersection>(std::pair<std::shared_ptr<Street>, std::shared_ptr<Street>>{smallPtr, mediumPtr});
-	// smallID = objManager.add<StraightStreet>(glm::vec3(25, 0, 0.01f), glm::vec3(-25, 50, 0.01f), "SmallStreet");
-	// smallPtr = std::dynamic_pointer_cast<StraightStreet>(objManager.get(smallID));
-	// objManager.add<Intersection>(std::pair<std::shared_ptr<Street>, std::shared_ptr<Street>>{smallPtr, mediumPtr});
-	// mediumID = objManager.add<StraightStreet>(glm::vec3(-25, 50, 0.02f), glm::vec3(-25, 0, 0.02f), "MediumStreet");
-	// mediumPtr = std::dynamic_pointer_cast<StraightStreet>(objManager.get(mediumID));
-	// objManager.add<Intersection>(std::pair<std::shared_ptr<Street>, std::shared_ptr<Street>>{smallPtr, mediumPtr});
-	// smallID = objManager.add<StraightStreet>(glm::vec3(-25, 0, 0.01f), glm::vec3(-75, 50, 0.01f), "SmallStreet");
-	// smallPtr = std::dynamic_pointer_cast<StraightStreet>(objManager.get(smallID));
-	// objManager.add<Intersection>(std::pair<std::shared_ptr<Street>, std::shared_ptr<Street>>{smallPtr, mediumPtr});
-	// mediumID = objManager.add<StraightStreet>(glm::vec3(-75, 50, 0.02f), glm::vec3(-50, 75, 0.02f), "MediumStreet");
-	// mediumPtr = std::dynamic_pointer_cast<StraightStreet>(objManager.get(mediumID));
-	// objManager.add<Intersection>(std::pair<std::shared_ptr<Street>, std::shared_ptr<Street>>{smallPtr, mediumPtr});
-	// smallID = objManager.add<StraightStreet>(glm::vec3(-50, 75, 0.01f), glm::vec3(0, 75, 0.01f), "SmallStreet");
-	// smallPtr = std::dynamic_pointer_cast<StraightStreet>(objManager.get(smallID));
-	// objManager.add<Intersection>(std::pair<std::shared_ptr<Street>, std::shared_ptr<Street>>{smallPtr, mediumPtr});
+	auto ID0 = objManager.add<StraightStreet>(glm::vec3(-50, -25, 0.01f), glm::vec3(0, -25, 0.01f), "SmallStreet");
+	auto ptr0 = std::dynamic_pointer_cast<StraightStreet>(objManager.get(ID0));
+	auto ID1 = objManager.add<StraightStreet>(glm::vec3(0, -25, 0.02f), glm::vec3(25, 0, 0.02f), "SmallStreet"/*"MediumStreet"*/);
+	auto ptr1 = std::dynamic_pointer_cast<StraightStreet>(objManager.get(ID1));
+	objManager.add<Intersection>(std::pair<std::shared_ptr<Street>, std::shared_ptr<Street>>{ptr0, ptr1});
+	auto ID2 = objManager.add<StraightStreet>(glm::vec3(25, 0, 0.01f), glm::vec3(-25, 50, 0.01f), "SmallStreet");
+	auto ptr2 = std::dynamic_pointer_cast<StraightStreet>(objManager.get(ID2));
+	objManager.add<Intersection>(std::pair<std::shared_ptr<Street>, std::shared_ptr<Street>>{ptr2, ptr1});
+	auto ID3 = objManager.add<StraightStreet>(glm::vec3(-25, 50, 0.02f), glm::vec3(-25, 0, 0.02f), /*"SmallStreet"*/"MediumStreet");
+	auto ptr3 = std::dynamic_pointer_cast<StraightStreet>(objManager.get(ID3));
+	objManager.add<Intersection>(std::pair<std::shared_ptr<Street>, std::shared_ptr<Street>>{ptr2, ptr3});
+	auto ID4 = objManager.add<StraightStreet>(glm::vec3(-25, 0, 0.01f), glm::vec3(-75, 50, 0.01f), "SmallStreet");
+	auto ptr4 = std::dynamic_pointer_cast<StraightStreet>(objManager.get(ID4));
+	objManager.add<Intersection>(std::pair<std::shared_ptr<Street>, std::shared_ptr<Street>>{ptr4, ptr3});
+	auto ID5 = objManager.add<StraightStreet>(glm::vec3(-75, 50, 0.02f), glm::vec3(-50, 75, 0.02f), "SmallStreet"/*"MediumStreet"*/);
+	auto ptr5 = std::dynamic_pointer_cast<StraightStreet>(objManager.get(ID5));
+	objManager.add<Intersection>(std::pair<std::shared_ptr<Street>, std::shared_ptr<Street>>{ptr4, ptr5});
+	auto ID6 = objManager.add<StraightStreet>(glm::vec3(-50, 75, 0.01f), glm::vec3(0, 75, 0.01f), "SmallStreet");
+	auto ptr6 = std::dynamic_pointer_cast<StraightStreet>(objManager.get(ID6));
+	objManager.add<Intersection>(std::pair<std::shared_ptr<Street>, std::shared_ptr<Street>>{ptr6, ptr5});
 	drawableManager.updateBuffer("SmallStreet");
 	drawableManager.updateBuffer("MediumStreet");
 }
--- src/objects/streets/intersection.cpp
+++ src/objects/streets/intersection.cpp
@@ -35,13 +35,10 @@ Intersection::Intersection(const std::pair<std::shared_ptr<Street>, std::shared_
 	const auto & dir2 = config2.getDirections();
 	const auto & lanes1 = config1.getLanes();
 	const auto & lanes2 = config2.getLanes();
-	// const auto & centers1 = config1.getLaneCenters();
-	// const auto & centers2 = config2.getLaneCenters();
-	if (lanes1 == lanes2) {
-		// same config -> no visible change!
-		// TO DO: connect paths with a node
-	} else if (dir1 == dir2) {
+
+	if (dir1 == dir2) {
 		// same directions and lane numbers
+
 		const auto dira = glm::vec2(m_tangents.front().x, m_tangents.front().y);
 		const auto dirb = -glm::vec2(m_tangents.back().x, m_tangents.back().y);
 		const auto anglea = glm::atan(dira.y, dira.x);
@@ -51,9 +48,9 @@ Intersection::Intersection(const std::pair<std::shared_ptr<Street>, std::shared_
 		if (angle < -glm::pi<float>()) angle += 2.f * glm::pi<float>();
 		if (angle > glm::pi<float>()) angle -= 2.f * glm::pi<float>();
 
-		const auto w1 = config1.getTotalWidth();
-		const auto w2 = config2.getTotalWidth();
-		const auto w = 0.5f * (w1 + w2);
+		const auto sameConfigs = lanes1 == lanes2;
+
+		const auto w = sameConfigs ? config1.getTotalWidth() : 0.5f * (config1.getTotalWidth() + config2.getTotalWidth());
 		const auto n1 = glm::normalize(glm::vec3(m_tangents.front().y, -m_tangents.front().x, 0.f));
 		const auto n2 = glm::normalize(glm::vec3(m_tangents.back().y, -m_tangents.back().x, 0.f));
 		const auto heightOffset = glm::vec3(0.f, 0.f, 0.03f);
@@ -63,63 +60,75 @@ Intersection::Intersection(const std::pair<std::shared_ptr<Street>, std::shared_
 		const auto centerOffset = (angle >= 0.f ? n1 : n2) * 0.5f * w;
 		const auto moveToPos = gameToGraphics(m_pos - centerOffset + heightOffset + tangentOffset);
 
-		auto _w1 = lanes1[0].width;
-		auto _w2 = lanes2[0].width;
-		auto _w = 0.5f * (_w1 + _w2);
-		// inner fan
-		{
+		auto addFan = [&](const glm::vec4 & color, const float scaling){
 			auto drawable = std::make_shared<Drawable>();
 			drawable->type = "IntersectionInner";
-			drawable->color = glm::vec4(0.5f * (lanes1[0].color + lanes2[0].color), 1.f);
+			drawable->color = color;
 			drawable->renderType = RenderTypeName::FAN;
 			drawable->misc = glm::vec4(std::abs(glm::degrees(angle)), 0.f, 0.f, 0.f);
 			drawable->dynamic = false;
 			drawable->unicolored = false;
-			const auto scaling = gameToGraphics(_w);
 			drawable->object.scale({scaling, scaling, 1.f});
 			drawable->object.rotate(rotateRadians, {0.f, 0.f, 1.f});
 			drawable->object.moveTo(moveToPos);
 			m_drawables.emplace_back(drawable);
-		}
-		// outer fans
-		for (auto i = 1u; i < lanes1.size(); ++i) {
-			const auto _w1_old = _w1;
-			const auto _w2_old = _w2;
-			_w1 += lanes1[i].width;
-			_w2 += lanes2[i].width;
-			_w = 0.5f * (_w1 + _w2);
+		};
+
+		auto addAnnulus = [&](const glm::vec4 & color, const float relOffset, const float scaling){
 			auto drawable = std::make_shared<Drawable>();
-			drawable = std::make_shared<Drawable>();
 			drawable->type = "IntersectionOuter";
-			drawable->color = glm::vec4(0.5f * (lanes1[i].color + lanes2[i].color), 1.f);
+			drawable->color = color;
 			drawable->renderType = RenderTypeName::ANNULUS;
-			const auto relOffset = (_w1_old / _w1 + _w2_old / _w2) * 0.5f;
 			drawable->misc = glm::vec4(std::abs(glm::degrees(angle)), relOffset, 0.f, 0.f);
 			drawable->dynamic = false;
 			drawable->unicolored = false;
-			const auto scaling = gameToGraphics(_w);
 			drawable->object.scale({scaling, scaling, 1.f});
 			drawable->object.rotate(rotateRadians, {0.f, 0.f, 1.f});
 			drawable->object.moveTo(moveToPos);
 			m_drawables.emplace_back(drawable);
+		};
+
+		auto _w1 = lanes1[0].width;
+		auto _w2 = lanes2[0].width;
+		auto _w = sameConfigs ? lanes1[0].width : 0.5f * (_w1 + _w2);
+		if (sameConfigs) {
+			addFan(glm::vec4(lanes1[0].color, 1.f), gameToGraphics(_w));
+		} else {
+			addFan(glm::vec4(0.5f * (lanes1[0].color + lanes2[0].color), 1.f), gameToGraphics(_w));
+		}
+		for (auto i = 1u; i < lanes1.size(); ++i) {
+			if (sameConfigs) {
+				const auto _w_old = _w;
+				_w += lanes1[i].width;
+				addAnnulus(glm::vec4(lanes1[i].color, 1.f), _w_old / _w, gameToGraphics(_w));
+			} else {
+				const auto _w1_old = _w1;
+				const auto _w2_old = _w2;
+				_w1 += lanes1[i].width;
+				_w2 += lanes2[i].width;
+				_w = 0.5f * (_w1 + _w2);
+				addAnnulus(glm::vec4(0.5f * (lanes1[i].color + lanes2[i].color), 1.f),
+						(_w1_old / _w1 + _w2_old / _w2) * 0.5f, gameToGraphics(_w));
+			}
 		}
 
-		// TO DO: different lane widths!
-		// TO DO: color smoothing
-		// TO DO: cut streets back by tangentoffset
-
+		const auto diff = sameConfigs ? 0.f : std::abs(config1.getTotalWidth() - config2.getTotalWidth()) * 0.5f;
 		if (glm::vec2(m_pos.x, m_pos.y) == m_streets.front()->getStartXY()) {
-			m_streets.front()->moveStart(2.f * x);
+			m_streets.front()->moveStart(x + diff);
 		} else {
-			m_streets.front()->moveEnd(-2.f * x);
+			m_streets.front()->moveEnd(-x - diff);
 		}
 		if (glm::vec2(m_pos.x, m_pos.y) == m_streets.back()->getStartXY()) {
-			m_streets.back()->moveStart(2.f * x);
+			m_streets.back()->moveStart(x + diff);
 		} else {
-			m_streets.back()->moveEnd(-2.f * x);
+			m_streets.back()->moveEnd(-x - diff);
 		}
+
+		// TO DO: connect streets with curve with a trapezoid
 
 	} else {
 		// TO DO: no uniform connections
 	}
+
+	// TO DO: connect paths with a node
 }
